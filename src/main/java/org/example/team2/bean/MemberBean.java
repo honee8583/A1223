bef8e695f664f0aa273972d470548c925b15e4a8
leedahoon <ekgns0508@gmail.com>
2024-12-23 12:43:17 +0900

    FEAT: MemberBean, MemberDAO

--- src/main/java/org/example/team2/bean/MemberBean.java
+++ src/main/java/org/example/team2/bean/MemberBean.java
@@ -1,5 +1,21 @@
 package org.example.team2.bean;
 
-public class MemberBean {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberBean {
+    private String id;
+    private String password;
+    private String name;
+    private char gender;
+    private String email;
+    private String birth;
+    private String zipcode;
+    private String address;
+    private String hobby;
+    private String job;
 }
